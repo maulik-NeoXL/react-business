6475e23dc94e8e0489229ebe224b14f6083d0879
maulik-NeoXL <maulik@neoxl.com>
2023-05-17 12:16:54 +0530

    Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,13 +3,13 @@ import {AiOutlineMenu, AiOutlineClose} from 'react-icons/ai'
 
 export function Navbar()
 {
-  const [open, setOpen] = useState(false);
+  const [nav, setNav] = useState(true);
 
-  const handleClick = () => {
-    setOpen(!open)
+  const handleNav = () => {
+    setNav(!nav)
   }
   return (
-    <div className='text-white flex items-center pr-4 pl-4 h-16 max-w-[1240px] mx-auto'>
+    <div className='text-white flex items-center pr-4 pl-4 h-17 max-w-[1240px] mx-auto'>
          <h1 className='w-full text-3xl font-bold text-[#00df9a] p-4'> React </h1>
       <ul className='flex hidden'>
         <li className='p-4'>Home </li>
@@ -18,10 +18,11 @@ export function Navbar()
         <li className='p-4'> About </li>
         <li className='p-4'> Contact </li>
       </ul>
-      <div>
-        <AiOutlineMenu size={24} className='hover:cursor-pointer'/>
+      <div onClick={handleNav} className='block md:hidden'>
+        {!nav ? <AiOutlineClose size = {24} className='cursor-pointer'/> : <AiOutlineMenu size={24} className='cursor-pointer'/> }
       </div>
-      <div className='fixed left-0 top-0 w-[30%] h-full border-r border-r-gray-900 bg-transparent' onClick={handleClick}>
+      <div className={!nav? 'fixed left-0 top-0 w-[30%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'fixed left-[-100%]'}>
+        <h1 className='w-full text-3xl font-bold text-[#00df9a] p-4 ml-9'> React </h1>
         <ul className='pt-24 uppercase'>
           <li className='p-4 border-b border-gray-500'>Home </li>
           <li className='p-4 border-b border-gray-500'> Company </li>
