9c1578b451c28f17b2a03dec67545bab66ff607e
maulik-NeoXL <maulik@neoxl.com>
2023-05-21 00:36:24 +0530

    Cards

--- src/components/Analytics.jsx
+++ src/components/Analytics.jsx
@@ -10,8 +10,8 @@ export default function Analytics()
         <div className='flex flex-col justify-center'>
           <p className='text-[#00df9a] font-bold md:text-xl text-md'> DATA ANALYTICS DASHBOARD</p>
           <h1 className='font-bold md:text-3xl sm:text-2xl mt-2'> Manage Data Analytics Centrally</h1>
-          <p className='mt-2'> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          <button className='p-2 bg-[#00df9a] font-bold md:w-[200px] w-[160px] rounded-md text-black mt-4'> Get Started </button>
+          <p className='mt-2 text-[#324963]'> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <button className='p-2 bg-[#00df9a] font-[500] md:w-[200px] w-[160px] rounded-md text-black mt-4'> Get Started </button>
         </div>
       </div>
     </div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,15 +7,15 @@ export default function Hero()
     <div className='text-white'>
          <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center">
            <p className='text-[#00df9a] font-bold'> GROWING WITH DATA ANALYTICS</p>
-           <h1 className='font-bold md:text-5xl text-4xl md:p-4 sm: py-4 '> Grow with Data.</h1>
+           <h1 className='font-bold md:text-6xl text-4xl md:p-4 sm: py-4 '> Groww with Data.</h1>
            <div>
              <p className='md: text-5xl sm: text-4xl text-xl font-bold'>Fast, flexible financing for <Typed className='md: text-5xl sm: text-4xl text-xl font-bold text-[#00df9a]' strings={['B2B', 'D2C', 'B2C', 'SAAS']}
                typeSpeed={120}
                backSpeed={150} loop/> </p>
            </div>
-           <p className='p-4 md:xl text-l font-bold text-gray-400 w-full'> Monitor your data analytics to increase revenue for B2B, D2C, B2C & SAAS platforms.</p>
+           <p className='p-4 md:xl text-l font-[500] text-gray-400 w-full'> Monitor your data analytics to increase revenue for B2B, D2C, B2C & SAAS platforms.</p>
            <div>
-             <button className='p-2 bg-[#00df9a] font-bold md:w-[200px] w-[160px] rounded-md text-black m-1'> Get Started </button>
+             <button className='p-2 bg-[#00df9a] font-[500] md:w-[200px] w-[160px] rounded-md text-white m-1'> Get Started </button>
            </div>
          </div>
     </div>
